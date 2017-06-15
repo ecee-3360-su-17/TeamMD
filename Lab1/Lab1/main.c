88325f0e9fe342174f12f00e868d127fd416cc0a
Derek Prince <ace.prince08@gmail.com>
2017-06-14 21:40:57 -0600

    Still fails to link.

--- Lab1/Lab1/main.c
+++ Lab1/Lab1/main.c
@@ -1,18 +1,35 @@
 
-
-/**
+/*
  * main.c
  */
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\inc\hw_types.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\inc\hw_gpio.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\inc\hw_memmap.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\inc\hw_sysctl.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\gpio.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\rom.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\sysctl.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\pin_map.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\can.h"
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\driverlib\timer.h"
 
 extern int fib(int n);
+void blink(char);
+void morse_conversion(int);
+void package_blink(char);
 
 int main(void){
-    //int fib_number = fib(5);
-	return 0;
+    int fib_number = fib(5);
+    morse_conversion(fib_number);
+    return 0;
 }
 
 void blink(char n) {
 /*    //n = pattern for me
+
     for(int i =0; i < NUM; i++){
         int state = ANOTHER_NUM;
         ___SET__LED__STATE___
@@ -20,6 +37,8 @@ void blink(char n) {
     }
     ___long wait between numbers___
 //testing new branch. That's all.*/
+    //Timer config
+    TimerConfigure(TIMER0_BASE, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_ONE_SHOT | TIMER_CFG_B_CAP_COUNT));
 }
 
 //come back to type. Will probably pass back array.
@@ -29,18 +48,16 @@ void morse_conversion(int n){
     int mod;
     while(n != 0){
         mod = n % 10;
-        blink(mod);
+        package_blink(mod); //note that this code might overwrite itself as one runs full-tilt and the morse output is timed.
         n = (n - mod)/10;
     }
 }
 
 void package_blink(char n){
     char coded_n = 0b11111;
-    if(n <= 5){
+    if(n <= 5)
         coded_n << (n-5);
-    }
-    else{
+    else
         coded_n >> n;
-    }
     blink(coded_n);
 }
