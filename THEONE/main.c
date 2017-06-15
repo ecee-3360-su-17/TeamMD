aef2aac3e18c9112f46215accc49cb2539777718
Derek Prince <ace.prince08@gmail.com>
2017-06-15 11:55:32 -0600

    It fuckin' werks, mate

--- THEONE/main.c
+++ THEONE/main.c
@@ -16,10 +16,16 @@
 #include "driverlib/can.h"
 #include "driverlib/timer.h"
 
+#define RED_LED GPIO_PIN_1
+#define BLUE_LED GPIO_PIN_2
+#define GREEN_LED GPIO_PIN_3
+
 extern int fib(int n);
 void blink(char);
 void morse_conversion(int);
 void package_blink(char);
+
+bool flag = 0;
 
 int main(void){
 
@@ -34,66 +40,121 @@ int main(void){
     {
     }
 
+
     //
     // Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
     // enable the GPIO pin for digital function.
     //
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, RED_LED|BLUE_LED|GREEN_LED);
 
-    //
-    // Turn on the LED.
-    //
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
 
-    //
-    // Delay for a bit.
-    //
-    for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++){}
-    //
-    // Turn off the LED.
-    //
-    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0x0);
+    //int fib_number = fib(5);
+    int i;
+    for(i = 11; i < 20; i++){
+        morse_conversion(i);
+    }
 
-    //
-    // Delay for a bit.
-    //
-    for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++){}
-    int fib_number = fib(5);
-    morse_conversion(fib_number);
     return 0;
 }
 
 void blink(char n) {
-/*    //n = pattern for me
+    char mask = 0b10000;    //change to char if not working
+    volatile uint32_t ui32Loop;
 
-    for(int i =0; i < NUM; i++){
-        int state = ANOTHER_NUM;
-        ___SET__LED__STATE___
-        ___WAIT between digits____
+    uint8_t i;
+    for(i = 0; i < 5; i++){
+        mask = 0b10000;
+        //
+        // Turn on the LED.
+        //
+        mask = mask & n;
+        n = n << 1;
+        if (mask == 0b00000){  //1 corresponds to a dash, 0 is dot
+            GPIOPinWrite(GPIO_PORTF_BASE, BLUE_LED, BLUE_LED);
+            for(ui32Loop = 0; ui32Loop < 400000; ui32Loop++){}
+        }
+        else{
+            GPIOPinWrite(GPIO_PORTF_BASE, GREEN_LED, GREEN_LED);
+            for(ui32Loop = 0; ui32Loop < 600000; ui32Loop++){}
+        }
+
+        //
+        // Turn off the LED.
+        //
+        GPIOPinWrite(GPIO_PORTF_BASE, BLUE_LED|GREEN_LED, 0x0);
+
+        //
+        // Delay for a bit.
+        //
+        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++){}
     }
-    ___long wait between numbers___
-//testing new branch. That's all.*/
-    //Timer config
-    TimerConfigure(TIMER0_BASE, (TIMER_CFG_SPLIT_PAIR | TIMER_CFG_A_ONE_SHOT | TIMER_CFG_B_CAP_COUNT));
+    GPIOPinWrite(GPIO_PORTF_BASE, RED_LED, RED_LED);
+    for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++){}
+    GPIOPinWrite(GPIO_PORTF_BASE, RED_LED, 0x0);
+    for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++){}
+    if(flag){
+        GPIOPinWrite(GPIO_PORTF_BASE, RED_LED, RED_LED);
+        for(ui32Loop = 0; ui32Loop < 800000; ui32Loop++){}
+    }
+    GPIOPinWrite(GPIO_PORTF_BASE, RED_LED, 0x0);
+    for(ui32Loop = 0; ui32Loop < 1000000; ui32Loop++){}
 }
 
 //come back to type. Will probably pass back array.
  //Depends on blink impl.
 void morse_conversion(int n){
     //takes in number, converts to array of bases.
+    flag = 0;   //flag is 1 when loop is complete. All numbers packaged. (i.e. 123 -> 1, 2, 3)
     int mod;
     while(n != 0){
         mod = n % 10;
         package_blink(mod); //note that this code might overwrite itself as one runs full-tilt and the morse output is timed.
         n = (n - mod)/10;
     }
+    flag = 1;
 }
 
 void package_blink(char n){
-    char coded_n = 0b11111;
-    if(n <= 5)
-        coded_n << (n-5);
+    /*  coded_n = 0b11111;
+        if(n <= 5)
+        coded_n = coded_n << (n-5);
     else
-        coded_n >> n;
-    blink(coded_n);
+        coded_n = coded_n >> n;
+    blink(coded_n);*/
+        switch(n){
+        case 0:
+            n = 0b11111;
+            break;
+        case 1:
+            n = 0b01111;
+            break;
+        case 2:
+            n = 0b00111;
+            break;
+        case 3:
+            n = 0b00011;
+            break;
+        case 4:
+            n = 0b00001;
+            break;
+        case 5:
+            n = 0b00000;
+            break;
+        case 6:
+            n = 0b10000;
+            break;
+        case 7:
+            n = 0b11000;
+            break;
+        case 8:
+            n = 0b11100;
+            break;
+        case 9:
+            n = 0b11110;
+            break;
+        default:
+            n = 0b10101;
+            break;
+        }
+        blink(n);
 }
