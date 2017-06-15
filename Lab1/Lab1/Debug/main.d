cddcf94d9a768a8866b0068c358a831db4174b21
Derek Prince <ace.prince08@gmail.com>
2017-06-15 09:17:01 -0600

    I have no idea what's in this commit. Wew

--- Lab1/Lab1/Debug/main.d
+++ Lab1/Lab1/Debug/main.d
@@ -1,1 +1,13 @@
-main.o: ../main.c
+main.o: ../main.c ../../../driverlib/gpio.h ../../../driverlib/rom.h \
+ ../../../driverlib/sysctl.h ../../../driverlib/pin_map.h \
+ ../../../driverlib/can.h
+
+../../../driverlib/gpio.h:
+
+../../../driverlib/rom.h:
+
+../../../driverlib/sysctl.h:
+
+../../../driverlib/pin_map.h:
+
+../../../driverlib/can.h:
--- Lab1/Lab1/main.c
+++ Lab1/Lab1/main.c
@@ -1,5 +1,16 @@
 
-
+//#include "../../driverlib/*"
+//#include "inc/hw_types.h"
+//#include "inc/hw_gpio.h"
+//#include "inc/hw_memmap.h"
+//#include "inc/hw_sysctl.h"
+#include "stdint.h"
+#include "stdbool.h"
+#include "../../driverlib/gpio.h"
+#include "../../driverlib/rom.h"
+#include "../../driverlib/sysctl.h"
+#include "../../driverlib/pin_map.h"
+#include "../../driverlib/can.h"
 /**
  * main.c
  */
